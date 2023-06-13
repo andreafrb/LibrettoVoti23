5f10a9f139848b1b49aee8d0893113e49e48ed6b
andreafrb <andreafrab@gmail.com>
2023-06-13 18:48:45 +0200

    Minuto 48 video L05

--- src/main/java/it/polito/tdp/libretto/model/Libretto.java
+++ src/main/java/it/polito/tdp/libretto/model/Libretto.java
@@ -1,5 +1,61 @@
 package it.polito.tdp.libretto.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Libretto {
+	
+	private ArrayList<Voto> voti;
+	
+	
+	public Libretto() {
+		this.voti = new ArrayList<Voto>();
+	}
+	
+	/**
+	 * Aggiungi nuovo voto al libretto
+	 * (Per ora nessun controllo)
+	 * @param v: Voto da aggiungere
+	 * @return true
+	 */
+
+	public boolean add(Voto v) {
+		return this.voti.add(v);
+	}
+	
+	public void stampa() {
+		for (Voto v: this.voti) {
+			System.out.println(v.toString());
+		}
+	}
+	
+	public void stampaPuntiUguali(int valore ) {
+		for (Voto v: this.voti) {
+			if (v.getPunti() == valore)
+				System.out.println(v.toString());
+		}
+	}
+	
+	public void stampaPuntiMaggiori(int valore ) {
+		for (Voto v: this.voti) {
+			if (v.getPunti() > valore)
+				System.out.println(v.toString());
+		}
+	}
+	
+	public void stampaPuntiMinori(int valore ) {
+		for (Voto v: this.voti) {
+			if (v.getPunti() < valore)
+				System.out.println(v.toString());
+		}
+	}
+	
+	public Voto cercaVotoPerNome(String corso) {
+		for (Voto v: this.voti) {
+			if (v.getCorso().equals(corso)) 
+				return v;	
+		}
+		return null;
+	}
 
 }
--- src/main/java/it/polito/tdp/libretto/model/TestLibretto.java
+++ src/main/java/it/polito/tdp/libretto/model/TestLibretto.java
@@ -1,9 +1,29 @@
 package it.polito.tdp.libretto.model;
 
+import java.time.LocalDate;
+
 public class TestLibretto {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Libretto lib = new Libretto();
+		
+		lib.add(new Voto("Analisi 1", 29, LocalDate.of(2023, 02, 15)));
+		lib.add(new Voto("Analisi 2", 25, LocalDate.of(2023, 06, 15)));
+		lib.add(new Voto("Fisica 1", 29, LocalDate.of(2023, 02, 1)));
+		lib.add(new Voto("Fisica 2", 27, LocalDate.of(2023, 01, 15)));
+		lib.add(new Voto("Geometria 5", 23, LocalDate.of(2023, 03, 15)));
+		
+		lib.stampa();
+		System.out.println("VOTO=25");
+		lib.stampaPuntiUguali(25);
+		System.out.println("VOTO>25");
+		lib.stampaPuntiMaggiori(25);
+		System.out.println("VOTO<25");
+		lib.stampaPuntiMinori(25);
+		
+		Voto v = lib.cercaVotoPerNome("Analisi 1");
+		System.out.println(v);
 
 	}
 
